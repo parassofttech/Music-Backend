d833a03984617e0464103e65bcffff900f95ba7d
Paras Sahu <parastop5468@gmail.com>
2026-01-06 18:44:14 +0530

    new change

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const   mongoose  = require("mongoose")
 const cors = require("cors")
 
 const contactRouter = require("./routers/contactRouter")
-const connectDB = require("./db")
+// const connectDB = require("./db")
 
 // const chekLogin = require("./middleware/chekLogin")
 
@@ -15,36 +15,36 @@ app.use(express.json())
 
 
 app.use(cors())
-connectDB()
+// connectDB()
 
-// let isConnected = false;
-// async function connectMongoDB() {
-//     try {
-//         await mongoose.cnnnect(process.env.DBURL, {
-//             useNewUrlParser: true,
-//             useUnifiedTopology: true
-//         });
-//         isConnected= true;
-//         console.log('connect to MongoDB')
-//     } catch(error){
-//         console.error('Error connecting to MongoDB', error);
-//     }
-// }
+let isConnected = false;
+async function connectMongoDB() {
+    try {
+        await mongoose.cnnnect(process.env.DBURL, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+        isConnected= true;
+        console.log('connect to MongoDB')
+    } catch(error){
+        console.error('Error connecting to MongoDB', error);
+    }
+}
 
-// app.use((req,res, next)=>{
-//     if(!isConnected){
-//         connectMongoDB()
-//     }
-//     next()
-// })
+app.use((req,res, next)=>{
+    if(!isConnected){
+        connectMongoDB()
+    }
+    next()
+})
 
 
 app.use("/music/api",contactRouter)
 app.use("/music/api",loginRouter)
 
-app.listen(process.env.PORT,()=>{
-    console.log("server is connected")
-})
+// app.listen(process.env.PORT,()=>{
+//     console.log("server is connected")
+// })
 
 
 module.exports = app
